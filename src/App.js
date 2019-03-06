b5d9af89bc4413450ea1b692ad22e33d431a544b
Akshay Sharma <akshay.fi@outlook.com>
2019-03-06 21:48:55 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Coursesales from "./Coursesales";
+
+class App extends Component {
+  render() {
+    var courses = [
+      { name: 'Complete ios10 dev course', price: 199},
+      { name: 'Complete pentesting course', price: 299},
+      { name: 'Complete front end course', price: 180},
+      { name: 'Bug Bounty and web app pentesting', price: 190},
+    ];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Course Purchase Page</h1>
+        </header>
+        <Coursesales items={courses}/>
+      </div>
+    );
+  }
+}
+
+export default App;
